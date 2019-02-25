4d5ee21660d63d8494afddeefed84ad32e8eac28
alex <alex@qr.com>
2019-02-25 17:47:14 +0800

    before ready to test

--- main.cpp
+++ main.cpp
@@ -17,31 +17,21 @@
 #include <iostream>
 #include <vector>
 
+#include "trigonometric.hpp"
+
 using namespace std;
 using namespace cv;
 
-typedef struct{
-	int a;
-	int b;
-	int c;
-}test_t;
-
-typedef struct{
-	Point2i ver;
-	Point2i pos;
-}position_t;
-
-bool comp(const position_t &a,const position_t &b)
-{
-	unsigned int tmpa ,tmpb;
-	tmpa = a.pos.x;
-	tmpb = b.pos.x;
-	return tmpa<tmpb;
-}
+using namespace cr_trigonometricInterpolation;
 
 int main(int argc , char** argv)
 {
 	//test_triangle();
+
+	Mat test = Mat(1080,1920,CV_8UC3);
+
+
+	Trigonometric tri = Trigonometric(1920,1080);
 
 	vector<position_t> verfp;
 
@@ -55,83 +45,34 @@ int main(int argc , char** argv)
 	 tmp.pos.y = 33768;
 	 verfp.push_back(tmp);
 
-	 tmp.ver.x = 2000;
-	 tmp.ver.y = 3000;
+	 tmp.ver.x = 300;
+	 tmp.ver.y = 0;
 	 tmp.pos.x = 1000;
 	 tmp.pos.y = 2000;
 	 verfp.push_back(tmp);
 
-	 tmp.ver.x = 3000;
-	 tmp.ver.y = 2000;
+	 tmp.ver.x = 150;
+	 tmp.ver.y = 300;
 	 tmp.pos.x = 2000;
 	 tmp.pos.y = 1000;
 	 verfp.push_back(tmp);
+
+	 tri.insertVertexAndPosition( verfp );
+	 tri.draw_subdiv( test , true );
+
+	 imshow("abcde" , test);
+	 cv::waitKey(10000);
+	 return 0;
+
+
 	 /****************************************/
-	 inpoint.x = 2000;
-	 inpoint.y = 2500;
 
-	 unsigned int d1, d2, d3 ,d;
-	 double f1, f2, f3 ,dtmp;
+	// inpoint.x = 150;
+	// inpoint.y = 200;
 
+	//int flag  =  tri.Point2getPos( inpoint,result );
 
-#if 0
-	 d1 = pow((inpoint.x - verfp[0].x),2) + pow((inpoint.y - verfp[0].y),2);
-	 d2 = pow((inpoint.x - verfp[1].x),2) + pow((inpoint.y - verfp[1].y),2);
-	 d3 = pow((inpoint.x - verfp[2].x),2) + pow((inpoint.y - verfp[2].y),2);
-	 d = d1 + d2 + d3;
-
-	 printf("d1 , d2, d3  ,d = (%u , %u ,%u , %u ) \n",d1, d2 ,d3 ,d );
-
-	f1 =  (double)d1/d;
-	f2 =  (double)d2/d;
-	f3 =  (double)d3/d;
-	printf("f1 , f2, f3  = (%f , %f ,%f)\n",f1, f2 ,f3);
-
-#else
-
-	int min = 40000 , max = 0;
-	int sizeNum = verfp.size();
-	if(sizeNum)
-		sort(verfp.begin(),verfp.end(),comp);
-
-	if(abs(verfp[2].pos.x - verfp[0].pos.x) > 18000)
-	{
-		verfp[0].pos.x += 36000;
-		if(verfp[1].pos.x < 18000)
-			verfp[1].pos.x += 36000;
-	}
-
-	for(int j=0;j<3;j++)
-	{
-		if( verfp[j].pos.y > 32000)
-		{
-			verfp[j].pos.y = 32768 - verfp[j].pos.y;
-		}
-	}
-
-	 d1 = pow((inpoint.x - verfp[0].ver.x),2) + pow((inpoint.y - verfp[0].ver.y),2);
-	 d2 = pow((inpoint.x - verfp[1].ver.x),2) + pow((inpoint.y - verfp[1].ver.y),2);
-	 d3 = pow((inpoint.x - verfp[2].ver.x),2) + pow((inpoint.y - verfp[2].ver.y),2);
-
-	 dtmp = 1+ (double)d1/d2 + (double)d2/d3 ;
-	 printf("d1 , d2, d3  = (%u , %u ,%u ) \n",d1, d2 ,d3 );
-	 f1 = 1/dtmp;
-	 f2 = (double)d1/d2*f1;
-	 f3 = 1 - f1 - f2;
-	 printf("f1 , f2, f3  = (%f , %f ,%f)\n",f1, f2 ,f3);
-
-#endif
-
-	 result.x = f1*verfp[0].pos.x + f2*verfp[1].pos.x + f3*verfp[2].pos.x;
-	 result.y = f1*verfp[0].pos.y + f2*verfp[1].pos.y + f3*verfp[2].pos.y;
-
-	printf("medium   result x,y = (%d , %d )\n",result.x, result.y);
-
-	result.x %= 36000;
-	if(result.y < 0)
-		result.y = 32768 - result.y ;
-
-	printf("result x,y = (%d , %d )\n",result.x, result.y);
+	//printf("flag = %d        ****   result x,y = %d ,%d \n",flag ,result.x,result.y);
 
 	return 0;
 }

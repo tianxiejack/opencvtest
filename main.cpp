adcac51be39fb107e4dcb036fc54852416ea6785
alex <alex@qr.com>
2019-03-15 11:45:05 +0800

    debugFor tri

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@
 #include "test_triangle.hpp"
 #include "stdio.h"
 #include "string.h"
-
+#include "stdlib.h"
 #include <algorithm>
 
 #include <iostream>
@@ -23,6 +23,30 @@ using namespace std;
 using namespace cv;
 
 using namespace cr_trigonometricInterpolation;
+FileStorage pointFs;
+
+int getpointFromYml(int& pointx , int & pointy )
+{
+	char paramName[40];
+	memset(paramName,0,sizeof(paramName));
+	string cfgFile;
+	cfgFile = "point.yml";
+	pointFs.open(cfgFile,FileStorage::READ);
+	int size ;
+
+	if(pointFs.isOpened())
+	{
+		sprintf(paramName,"inPointx");
+		pointFs[paramName] >>pointx;
+		sprintf(paramName,"inPointy");
+		pointFs[paramName] >>pointy;
+
+		pointFs.release();
+		return true;
+	}
+	return false;
+}
+
 
 int main(int argc , char** argv)
 {
@@ -41,35 +65,44 @@ int main(int argc , char** argv)
 
 	 tri.readParams(verfp);
 	 tri.insertVertexAndPosition( verfp );
-	 //tri.writeParams();
 
-	 //vector<position_t> fp88;
-	 //tri.readParams(fp88);
+#if 0
+	 int tmpnum;
+	 for(int k=0; k < 50; k++)
+	 {
+		 tmpnum = rand()%100;
+		 if(tmpnum < verfp.size())
+			 verfp.erase(verfp.begin()+tmpnum);
+		 else
+			 verfp.erase(verfp.begin()+tmpnum%10);
+	 }
+	 tri.insertVertexAndPosition( verfp );
 
-	// for(int k =0 ;k < fp88.size();k++)
-	 //		printf("orignal   ver -x,y = (%d , %d )  , pos-x,y  = (%d ,%d )\n",fp88[k].ver.x,fp88[k].ver.y, fp88[k].pos.x, fp88[k].pos.y);
-
+	 tri.writeParams();
+	 return 0;
+#endif
 
 #if 1
 	 tri.draw_subdiv( test , true );
 
 	 /****************************************/
-
-	inpoint.x = 1300;
-	inpoint.y = 800;
-
+	putchar(10);putchar(10);putchar(10);
+	inpoint.x = 800;
+	inpoint.y = 860;
+	getpointFromYml(inpoint.x,inpoint.y);
+	printf("inpoint  :x,y =  (%d , %d ) \n" ,inpoint.x , inpoint.y );
 
 	vector<position_t> aaa;
 	Point2i getpos;
 	tri.draw_point_triangle( test , inpoint ,aaa,getpos ,true );
 
-	//for(int k =0 ;k < aaa.size();k++)
-	//	printf("orignal   ver -x,y = (%d , %d )  , pos-x,y  = (%d ,%d )\n",aaa[k].ver.x,aaa[k].ver.y, aaa[k].pos.x, aaa[k].pos.y);
-	//printf( "getpos-x,y = (%d , %d ) \n",getpos.x , getpos.y );
+	for(int k =0 ;k < aaa.size();k++)
+		printf("orignal   ver -x,y = (%d , %d )  , pos-x,y  = (%d ,%d )\n",aaa[k].ver.x,aaa[k].ver.y, aaa[k].pos.x, aaa[k].pos.y);
+	printf( "getpos-x,y = (%d , %d ) \n",getpos.x , getpos.y );
 
 	imshow("111111111111", test);
-	cv::waitKey(30000);
-
+	cv::waitKey(3000);
+	putchar(10);putchar(10);putchar(10);
 	//int flag  =  tri.Point2getPos( inpoint,result );
 
 	//printf("flag = %d        ****   result x,y = %d ,%d \n",flag ,result.x,result.y);
